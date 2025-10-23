b19d02b0530471d4eb3c3ad39abe6d60fdaeb21e
Secre <yongcomarc6@gmail.com>
2025-10-23 18:36:32 +0800

    fix the DoctorSection

--- WellAll_app/resources/views/DoctorEdit.blade.php
+++ WellAll_app/resources/views/DoctorEdit.blade.php
@@ -5,35 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Doctor</title>
-    @vite(['resources/css/NavigationStyle.css'])
-
+    @vite(['resources/css/DoctorEditStyle.css'])
 </head>
 <body>
-    <h1>Edit Doctor: {{ $doctor->DoctorFirstName }} {{ $doctor->DoctorLastName }}</h1>
+    <main>
+        <h1>Edit Doctor: {{ $doctor->DoctorFirstName }} {{ $doctor->DoctorLastName }}</h1>
 
-    @if($errors->any())
-        <div style="color: red;">
-            <ul>
+        {{-- Error messages --}}
+        @if($errors->any())
+            <ul class="error-list">
                 @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
             </ul>
-        </div>
-    @endif
+        @endif
 
-    <form action="{{ route('updateDoctor', $doctor->DoctorID) }}" method="POST">
-        @csrf
-        @method('PUT')
+        {{-- Form --}}
+        <form action="{{ route('updateDoctor', $doctor->DoctorID) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-        <input type="text" name="Specialization" value="{{ $doctor->Specialization }}">
-        <input type="text" name="DoctorAvailability" value="{{ $doctor->DoctorAvailability }}">
-        <input type="text" name="DoctorContactNumber" value="{{ $doctor->DoctorContactNumber }}">
-        <input type="text" name="DoctorEmail" value="{{ $doctor->DoctorEmail }}">
-        <input type="text" name="DoctorAddress" value="{{ $doctor->DoctorAddress }}">
-        <button type="submit">Save Changes</button>
-    </form>
+            <label>Specialization</label>
+            <input type="text" name="Specialization" value="{{ $doctor->Specialization }}" required>
 
-    <br>
-    <a href="{{ route('DoctorSection') }}">Back to list</a>
+            <label>Availability</label>
+            <input type="text" name="DoctorAvailability" value="{{ $doctor->DoctorAvailability }}" required>
+
+            <label>Contact Number</label>
+            <input type="text" name="DoctorContactNumber" value="{{ $doctor->DoctorContactNumber }}" required>
+
+            <label>Email</label>
+            <input type="text" name="DoctorEmail" value="{{ $doctor->DoctorEmail }}" required>
+
+            <label>Address</label>
+            <input type="text" name="DoctorAddress" value="{{ $doctor->DoctorAddress }}" required>
+
+            <button type="submit">Save Changes</button>
+        </form>
+
+        <a href="{{ route('DoctorSection') }}"> Back to list</a>
+    </main>
 </body>
 </html>
--- WellAll_app/resources/views/DoctorView.blade.php
+++ WellAll_app/resources/views/DoctorView.blade.php
@@ -4,23 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href='https://fonts.googleapis.com/css?family=Libre Barcode 39' rel='stylesheet'>
     <title>Doctor Details</title>
-    @vite(['resources/css/NavigationStyle.css'])
+    <link href='https://fonts.googleapis.com/css?family=Libre Barcode 39' rel='stylesheet'>
+    @vite(['resources/css/DoctorViewStyle.css'])
 </head>
 <body>
-    <h1>Doctor: {{ $doctor->DoctorFirstName }} {{ $doctor->DoctorLastName }}</h1>
-    <p style="font-family: 'Libre Barcode 39'; font-size: 40px;">{{ $doctor->DoctorBarcode }}</p>
+    <main>
+        <h1>Doctor: {{ $doctor->DoctorFirstName }} {{ $doctor->DoctorLastName }}</h1>
 
-    <ul>
-        <li><strong>Doctor Code:</strong> {{ $doctor->DoctorBarcode }}</li>
-        <li><strong>Specialization:</strong> {{ $doctor->Specialization }}</li>
-        <li><strong>Contact:</strong> {{ $doctor->DoctorContactNumber }}</li>
-        <li><strong>Email:</strong> {{ $doctor->DoctorEmail }}</li>
-        <li><strong>Address:</strong> {{ $doctor->DoctorAddress }}</li>
-        <li><strong>Date Registered:</strong> {{ $doctor->DoctorDateRegistered }}</li>
-    </ul>
+        <p class="barcode">{{ $doctor->DoctorBarcode }}</p>
 
-    <a href="{{ route('DoctorSection') }}">Back to list</a>
+        <div class="doctor-info">
+            <ul>
+                <li><strong>Doctor Code:</strong> {{ $doctor->DoctorBarcode }}</li>
+                <li><strong>Specialization:</strong> {{ $doctor->Specialization }}</li>
+                <li><strong>Contact:</strong> {{ $doctor->DoctorContactNumber }}</li>
+                <li><strong>Email:</strong> {{ $doctor->DoctorEmail }}</li>
+                <li><strong>Address:</strong> {{ $doctor->DoctorAddress }}</li>
+                <li><strong>Date Registered:</strong> {{ $doctor->DoctorDateRegistered }}</li>
+            </ul>
+        </div>
+
+        <a href="{{ route('DoctorSection') }}">Back to list</a>
+    </main>
 </body>
 </html>
